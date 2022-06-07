39a09ab9e9adf98ee24321ac69aeda8329aaef5a
Paulo Bruno <pbrunosousa@gmail.com>
2022-06-07 15:04:30 +0200

    Pop1 composition

--- src/pop1/odesystem.jl
+++ src/pop1/odesystem.jl
@@ -1,40 +1,79 @@
-include("../common_pop/parameters.jl")
+using Interpolations, ModelingToolkit, DifferentialEquations
+
+# this was necessary because t0 is needed in initialisations.jl
+global t0 = 1900
+
+include("../functions.jl")
 include("../common_pop/tables.jl")
+include("../common_pop/parameters.jl")
 include("../common_pop/initialisations.jl")
 include("../common_pop/odesystem.jl")
 
-# Equations
-global eqs = [
-    # POPULATION LEVEL EQUATIONS
-    D(pop) ~ br - dr, # line 1 page 167
-    # DEATH RATE EQUATIONS
-    dr ~ pop / le, # line 4 page 167
-    # BIRTH RATE EQUATIONS
-    br ~ clip(dr, tf * pop * ffw / rlt, t, pet) # line 28 page 168
+
+function Pop1(; name)
+    @parameters ffw = ffwv rlt = rltv pet = petv
+
+    @variables le(t) tf(t)
+    @variables pop(t) = pop0
+    @variables dr(t) br(t)
+
+    eqs = [
+        D(pop) ~ br - dr,
+        dr ~ pop / le,
+        br ~ clip(dr, tf * pop * ffw / rlt, t, pet)
+    ]
+
+    ODESystem(eqs; name)
+end
+
+
+@named pop = Pop1()
+
+connection_eqs = [
+    pop.le ~ dr.le
+    pop.tf ~ br.tf
+    # common pop
+    dr.dr ~ pop.dr
+    dr.pop ~ pop.pop
+    br.br ~ pop.br
+    br.pop ~ pop.pop
+    io.pop ~ pop.pop
+    so.pop ~ pop.pop
+    f.pop ~ pop.pop
 ]
-eqs = vcat(eqs, common_eqs)
-# Initialisations
-global u0 = [
-    pop => pop0, # lines 2-3 page 167
-]
-u0 = vcat(u0, common_u0)
-# Parameters
-global p = common_p
-# ODE solution plot
+
+
+connection_eqs = vcat(connection_eqs, common_connection_eqs)
+# u0 = vcat(u0, common_u0)
+# global p = common_p
+
+# @named _cpop_model = ODESystem(cpop.connection_eqs, t)
+# @named cpop_model = compose(_cpop_model, [cpop.dr, cpop.br, cpop.io, cpop.so, cpop.pp, cpop.f])
+
+@named _pop_model = ODESystem(connection_eqs, t)
+@named pop_model = compose(_pop_model, [pop, dr, br, io, so, pp, f])
+
+pop_sys = structural_simplify(pop_model)
+
+prob = ODEProblem(pop_sys, [], (1900, 2100.0))
+sol = solve(prob, Tsit5())
+
+
+
 function plot_sol_2_84(sol)
     traces = GenericTrace[]
-    push!(traces, scatter(x=sol[t], y=sol[pop], name="pop", yaxis="y1"))
-    push!(traces, scatter(x=sol[t], y=sol[cbr], name="cbr", yaxis="y2"))
-    push!(traces, scatter(x=sol[t], y=sol[cdr], name="cdr", yaxis="y3"))
-    push!(traces, scatter(x=sol[t], y=sol[sopc], name="sopc", yaxis="y4"))
-    push!(traces, scatter(x=sol[t], y=sol[fpu], name="fpu", yaxis="y5"))
-    push!(traces, scatter(x=sol[t], y=sol[fpc], name="fpc", yaxis="y6"))
-    push!(traces, scatter(x=sol[t], y=sol[le], name="le", yaxis="y7"))
-    push!(traces, scatter(x=sol[t], y=sol[fce], name="fce", yaxis="y8"))
-    push!(traces, scatter(x=sol[t], y=sol[iopc], name="iopc", yaxis="y9"))
+    push!(traces, scatter(x=sol[t], y=sol[pop.pop], name="pop", yaxis="y1"))
+    push!(traces, scatter(x=sol[t], y=sol[br.cbr],  name="cbr", yaxis="y2"))
+    push!(traces, scatter(x=sol[t], y=sol[dr.cdr],  name="cdr", yaxis="y3"))
+    push!(traces, scatter(x=sol[t], y=sol[so.sopc], name="sopc", yaxis="y4"))
+    push!(traces, scatter(x=sol[t], y=sol[dr.fpu],  name="fpu", yaxis="y5"))
+    push!(traces, scatter(x=sol[t], y=sol[f.fpc],   name="fpc", yaxis="y6"))
+    push!(traces, scatter(x=sol[t], y=sol[dr.le],   name="le", yaxis="y7"))
+    push!(traces, scatter(x=sol[t], y=sol[br.fce],  name="fce", yaxis="y8"))
+    push!(traces, scatter(x=sol[t], y=sol[io.iopc], name="iopc", yaxis="y9"))
     plot(traces,
         Layout(xaxis_domain=[0.3, 0.7],
-            yaxis=attr(title="pop", range=[0, 1.6e10]),
+            yaxis =attr(title="pop", range=[0, 1.6e10]),
             yaxis2=attr(title="cbr", overlaying="y", side="right", position=0.70, range=[0, 50]),
             yaxis3=attr(title="cdr", overlaying="y", side="right", position=0.74, range=[0, 50]),
             yaxis4=attr(title="sopc", overlaying="y", side="right", position=0.78, range=[0, 1000]),
