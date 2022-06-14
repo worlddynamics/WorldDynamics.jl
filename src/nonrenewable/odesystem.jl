76735ecfd105ff72b97f10afe6b99e4b1093e714
Paulo Bruno <pbrunosousa@gmail.com>
2022-06-14 10:56:12 +0200

    Separate non-renewable system from functions

--- src/nonrenewable/odesystem.jl
+++ src/nonrenewable/odesystem.jl
@@ -1,3 +1,8 @@
+module NonRenewable
+
+export non_renewable
+
+
 using Interpolations, ModelingToolkit, DifferentialEquations
 
 include("../functions.jl")
@@ -16,7 +21,7 @@ include("parameters.jl")
 D = Differential(t)
 
 
-function Population(; name)
+function population(; name)
     @parameters gc = gcv pop2 = pop2v popi = popiv zpgt = zpgtv
 
     @variables pop(t), pop1(t)
@@ -29,7 +34,7 @@ function Population(; name)
     ODESystem(eqs; name)
 end
 
-function Industrial_Output(; name)
+function industrial_output(; name)
     @parameters icor = icorv
     
     @variables ic(t) fcaor(t) pop(t)
@@ -43,7 +48,7 @@ function Industrial_Output(; name)
     ODESystem(eqs; name)
 end
 
-function Industrial_Capital(; name)
+function industrial_capital(; name)
     @parameters fioaa = fioaav fioas = fioasv fioac = fioacv alic = alicv
 
     @variables io(t)
@@ -59,7 +64,7 @@ function Industrial_Capital(; name)
     ODESystem(eqs; name)
 end
 
-function Non_Renewable(; name)
+function non_renewable(; name)
     @parameters nri = nriv nruf1 = nruf1v nruf2 = nruf2v pyear = pyearv
 
     @variables pop(t) pcrum(t) iopc(t)
@@ -80,46 +85,5 @@ function Non_Renewable(; name)
     ODESystem(eqs; name)
 end
 
-
-@named pop = Population()
-@named io = Industrial_Output()
-@named ic = Industrial_Capital()
-@named nr = Non_Renewable()
-
-connection_eqs = [
-    nr.pop ~ pop.pop
-    nr.iopc ~ io.iopc
-    nr.fcaor ~ io.fcaor
-    io.ic ~ ic.ic
-    io.pop ~ pop.pop
-    ic.io ~ io.io
-]
-
-@named _nr_model = ODESystem(connection_eqs, t)
-@named nr_model = compose(_nr_model, [nr, pop, io, ic])
-
-nr_sys = structural_simplify(nr_model)
-
-prob = ODEProblem(nr_sys, [], (1900, 2100.0))
-sol = solve(prob, Tsit5())
-
-
-using PlotlyJS
-
-function plot_sol_5_25(sol)
-    traces = GenericTrace[]
-    push!(traces, scatter(x=sol[t], y=sol[nr.nrfr],  name="nrfr",  yaxis="y1"))
-    push!(traces, scatter(x=sol[t], y=sol[nr.fcaor], name="fcaor", yaxis="y2"))
-    push!(traces, scatter(x=sol[t], y=sol[ic.ic],    name="ic",    yaxis="y3"))
-    push!(traces, scatter(x=sol[t], y=sol[io.io],    name="io",    yaxis="y4"))
-    push!(traces, scatter(x=sol[t], y=sol[pop.pop],  name="pop",   yaxis="y5"))
-    plot(traces,
-        Layout(xaxis_domain=[0.3, 0.7],
-            yaxis  = attr(title="nrfr", range=[0, 1]),
-            yaxis2 = attr(title="fcaor", overlaying="y", side="right", position=0.70, range=[0, 1]),
-            yaxis3 = attr(title="ic",    overlaying="y", side="right", position=0.74, range=[0, 40e12]),
-            yaxis4 = attr(title="io",    overlaying="y", side="right", position=0.78, range=[0, 10e12]),
-            yaxis5 = attr(title="pop",   overlaying="y", side="right", position=0.82, range=[0, 16e9]),
-        )
-    )
-end
+    
+end # module
