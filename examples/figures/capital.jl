d3949dc14118405b14ce22b776a59def5025258b
Paulo Bruno <pbrunosousa@gmail.com>
2022-07-05 10:44:42 +0200

    Create figure 3.37

--- examples/figures/capital.jl
+++ examples/figures/capital.jl
@@ -9,6 +9,9 @@ sol = capital_historicalrun()
 @named pop = WorldDynamics.World3.Capital.population()
 @named ag = WorldDynamics.World3.Capital.agriculture()
 @named nr = WorldDynamics.World3.Capital.non_renewable()
+@named is = WorldDynamics.World3.Capital.industrial_subsector()
+@named ss = WorldDynamics.World3.Capital.service_subsector()
+@named js = WorldDynamics.World3.Capital.job_subsector()
 
 @variables t
 
@@ -22,3 +25,14 @@ fig_3_36_variables = [
 ]
 
 plotvariables(sol, (t, 1900.0, 1970.0), fig_3_36_variables, name="Fig. 3.36", showlegend=true, showaxis=true, colored=true)
+
+
+fig_3_37_variables = [
+    (is.iopc,  0, 1000, "iopc"),
+    (ss.sopc,  0, 1000, "sopc"),
+    (is.io,    0, 4e12, "io"),
+    (ss.fioas, 0, 0.4, "fioas"),
+    (js.cuf,   0, 1, "cuf"),
+]
+
+plotvariables(sol, (t, 1900.0, 2000.0), fig_3_37_variables, name="Fig. 3.37", showlegend=true, showaxis=true, colored=true)
