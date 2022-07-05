2d8b0aee39c53642f88280801f0f72345a4f7955
Paulo Bruno <pbrunosousa@gmail.com>
2022-07-05 10:48:36 +0200

    Reproduce figure 5.29

--- examples/figures/nonrenewable.jl
+++ examples/figures/nonrenewable.jl
@@ -31,3 +31,11 @@ parameters_5_26[:nri] = 2e12
 sol_5_26 = nonrenewable_historicalrun(params=parameters_5_26)
 
 plotvariables(sol_5_26, (t, 1900.0, 2100.0), fig_5_25_variables, name="Fig. 5.26", showlegend=true, showaxis=true, colored=true)
+
+
+parameters_5_29 = copy(WorldDynamics.World3.NonRenewable.params)
+parameters_5_29[:nruf2] = 0.25
+
+sol_5_29 = nonrenewable_historicalrun(params=parameters_5_29)
+
+plotvariables(sol_5_29, (t, 1900.0, 2100.0), fig_5_25_variables, name="Fig. 5.29", showlegend=true, showaxis=true, colored=true)
