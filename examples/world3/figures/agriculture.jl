62cd45b589a0129faf0a1008cde3777661de42bf
Paulo Bruno <pbrunosousa@gmail.com>
2022-09-12 15:52:38 +0200

    Remove external namespace from Agriculture example

--- examples/world3/figures/agriculture.jl
+++ examples/world3/figures/agriculture.jl
@@ -7,10 +7,10 @@ system = agriculture_historicalrun()
 sol = WorldDynamics.solve(system, (1900, 2100))
 
 
-@named ld = WorldDynamics.World3.Agriculture.land_development()
-@named ai = WorldDynamics.World3.Agriculture.agricultural_inputs()
-@named lfd = WorldDynamics.World3.Agriculture.land_fertility_degradation()
-@named leuiu = WorldDynamics.World3.Agriculture.land_erosion_urban_industrial_use()
+@named ld = World3.Agriculture.land_development()
+@named ai = World3.Agriculture.agricultural_inputs()
+@named lfd = World3.Agriculture.land_fertility_degradation()
+@named leuiu = World3.Agriculture.land_erosion_urban_industrial_use()
 
 @variables t
 
@@ -33,19 +33,17 @@ fig_4_69b_variables = [
 ]
 
 plotvariables(sol, (t, 1900, 1970), fig_4_69a_variables, name="Fig. 4.69a", showlegend=true, showaxis=true, colored=true)
-
 plotvariables(sol, (t, 1900, 1970), fig_4_69b_variables, name="Fig. 4.69b", showlegend=true, showaxis=true, colored=true)
 
 
-parameters_4_74 = WorldDynamics.World3.Agriculture.getparameters()
+parameters_4_74 = World3.Agriculture.getparameters()
 parameters_4_74[:palt] = 4.35e9
 
-initialisations_4_74 = WorldDynamics.World3.Agriculture.getinitialisations()
+initialisations_4_74 = World3.Agriculture.getinitialisations()
 initialisations_4_74[:pal] = 3.45e9
 
 system = agriculture_historicalrun(params=parameters_4_74, inits=initialisations_4_74)
 sol_4_74 = WorldDynamics.solve(system, (1900, 2100))
 
 plotvariables(sol_4_74, (t, 1900, 2100), fig_4_69a_variables, name="Fig. 4.74a", showlegend=true, showaxis=true, colored=true)
-
 plotvariables(sol_4_74, (t, 1900, 2100), fig_4_69b_variables, name="Fig. 4.74b", showlegend=true, showaxis=true, colored=true)
