235c677b0a4459a5a1f356902d6882f8c7e245b0
Paulo Bruno <pbrunosousa@gmail.com>
2022-09-12 18:19:52 +0200

    Reproduce fig 4.90

--- examples/world3/figures/agriculture.jl
+++ examples/world3/figures/agriculture.jl
@@ -104,3 +104,13 @@ sol_4_89 = WorldDynamics.solve(system, (1900, 2100))
 
 plotvariables(sol_4_89, (t, 1900, 2100), fig_4_69a_variables, name="Fig. 4.89a", showlegend=true, showaxis=true, colored=true)
 plotvariables(sol_4_89, (t, 1900, 2100), fig_4_69b_variables, name="Fig. 4.89b", showlegend=true, showaxis=true, colored=true)
+
+
+parameters_4_90 = World3.Agriculture.getparameters()
+parameters_4_90[:eyear] = 2000
+
+system = agriculture_historicalrun(params=parameters_4_90)
+sol_4_90 = WorldDynamics.solve(system, (1900, 2100))
+
+plotvariables(sol_4_90, (t, 1900, 2100), fig_4_69a_variables, name="Fig. 4.90a", showlegend=true, showaxis=true, colored=true)
+plotvariables(sol_4_90, (t, 1900, 2100), fig_4_69b_variables, name="Fig. 4.90b", showlegend=true, showaxis=true, colored=true)
