247a3a417d238f1990e921f83d13ccc04f87d5ed
Paulo Bruno <pbrunosousa@gmail.com>
2022-09-13 11:18:43 +0200

    Simplify interpolate condition comparison

--- src/functions.jl
+++ src/functions.jl
@@ -2,11 +2,11 @@ using Interpolations
 
 function interpolate(x::Float64, y::Tuple{Vararg{Float64}}, xs::Tuple{Float64, Float64})::Float64
     expanded_xs = LinRange(xs[1], xs[2], length(y))
-    if (x < expanded_xs[1])
+    if (x <= xs[1])
         return y[1]
     end
-    if (x > expanded_xs[length(expanded_xs)])
-        return y[length(expanded_xs)]
+    if (x >= xs[2])
+        return y[end]
     end
     li = LinearInterpolation(expanded_xs, collect(y))
     return li(x)
