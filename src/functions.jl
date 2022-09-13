b45cd29b8abbd5131301b7770fb9a0ffd82563fc
Paulo Bruno <pbrunosousa@gmail.com>
2022-09-13 16:55:13 +0200

    Branchless version of interpolate function
    Also removed the Interpolations package, since now we are computing the
    interpolation by hand.

--- src/functions.jl
+++ src/functions.jl
@@ -1,16 +1,19 @@
-using Interpolations
 using IfElse
 
+interpolate(x, x₁, xₙ, y₁, yₙ) = y₁ + (x - x₁) * ((yₙ - y₁) / (xₙ - x₁))
 function interpolate(x::Float64, yvalues::Tuple{Vararg{Float64}}, xrange::Tuple{Float64, Float64})::Float64
-    expanded_xrange = LinRange(xrange[1], xrange[2], length(yvalues))
-    if (x <= xrange[1])
-        return yvalues[1]
+    xvalues = LinRange(xrange[1], xrange[2], length(yvalues))
+
+    # y gets the min y value if less than the min x range
+    #   or the max y value if greater than the max x range
+    y = (x < xrange[1]) * yvalues[1] + (x > xrange[2]) * yvalues[end]
+
+    # in case x is inside the range, y gets the interpolated value
+    for i ∈ 1:length(yvalues)-1
+        y += (x ≥ xvalues[i]) * (x ≤ xvalues[i+1]) * interpolate(x, xvalues[i], xvalues[i+1], yvalues[i], yvalues[i+1])
     end
-    if (x >= xrange[2])
-        return yvalues[end]
-    end
-    li = LinearInterpolation(expanded_xrange, collect(yvalues))
-    return li(x)
+
+    return y
 end
 
 clip(returnifgte::Float64, returniflt::Float64, inputvalue::Float64, threshold::Float64) = IfElse.ifelse(inputvalue ≥ threshold, returnifgte, returniflt)
