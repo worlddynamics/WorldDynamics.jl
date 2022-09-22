4235f9e66d7c8fe490a766d7bd2e74a1f1448203
Paulo Bruno <pbrunosousa@gmail.com>
2022-09-22 14:31:34 +0200

    Change default values of plotvariables function
    Since all figures from the book are plotted with axes and colored
    lines, it makes sense that these are the default values.

--- src/plotvariables.jl
+++ src/plotvariables.jl
@@ -20,11 +20,11 @@ function plotvariables(solution, xrange, variables::Vector{<:NTuple{3, Any}}; kw
 end
 
 """
-   `plotvariables(solution, xrange, variables::Vector{<:NTuple{4, Any}}; name="", showaxis=false, showlegend=true, linetype="lines", colored=false)`
+   `plotvariables(solution, xrange, variables::Vector{<:NTuple{4, Any}}; name="", showaxis=true, showlegend=true, linetype="lines", colored=true)`
 
 Plot the values of the variables in the vector `variables` obtained by the ODE system `solution` (normally, obtained by using the `solve` function in `solvesystems.jl`) in the specified `xrange` interval. For each variable, the vector `variables` includes a quadruple, containing the Julia variable, its range, and its symbolic name to be shown in the plot.
 """
-function plotvariables(solution, xrange, variables::Vector{<:NTuple{4, Any}}; name="", showaxis=false, showlegend=true, linetype="lines", colored=false, save=false)
+function plotvariables(solution, xrange, variables::Vector{<:NTuple{4, Any}}; name="", showaxis=true, showlegend=true, linetype="lines", colored=true, save=false)
     numvars = length(variables)
 
     @assert 1 ≤ numvars
