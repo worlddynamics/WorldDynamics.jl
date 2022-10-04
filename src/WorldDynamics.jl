9d2e878c3efe61823b51f431061cc71f863d0074
Paulo Bruno <pbrunosousa@gmail.com>
2022-10-04 17:03:53 +0200

    Remove step function export

--- src/WorldDynamics.jl
+++ src/WorldDynamics.jl
@@ -6,14 +6,14 @@ include("functions.jl")
 
 export plotvariables
 export solve
-export interpolate, clip, switch, step
+export interpolate, clip, switch
 
 using ModelingToolkit
 
 @register interpolate(x, y::Tuple{Vararg{Float64}}, xs::Tuple{Float64, Float64})
 @register clip(f1, f2, va, th)
 @register switch(v1, v2, z)
-@register step(t, hght, sttm)
+@register WorldDynamics.step(t, hght, sttm)
 
 include("World2/World2.jl")
 include("World3/World3.jl")
