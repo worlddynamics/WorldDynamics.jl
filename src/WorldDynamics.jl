f132ff52319fdfd8768d84bca4bcadb5fbe8710d
Paulo Bruno <pbrunosousa@gmail.com>
2022-10-04 16:02:04 +0200

    Register functions in global module

--- src/WorldDynamics.jl
+++ src/WorldDynamics.jl
@@ -1,10 +1,19 @@
 module WorldDynamics
 
-include("functions.jl")
 include("plotvariables.jl")
 include("solvesystems.jl")
+include("functions.jl")
 
-export interpolate, clip, switch, step, plotvariables, solve
+export plotvariables
+export solve
+export interpolate, clip, switch, step
+
+using ModelingToolkit
+
+@register interpolate(x, y::Tuple{Vararg{Float64}}, xs::Tuple{Float64, Float64})
+@register clip(f1, f2, va, th)
+@register switch(v1, v2, z)
+@register step(t, hght, sttm)
 
 include("World2/World2.jl")
 include("World3/World3.jl")
