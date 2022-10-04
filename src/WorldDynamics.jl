43a8d521e7f2778804b98dd659ece7f9f6223ed6
Paulo Bruno <pbrunosousa@gmail.com>
2022-10-04 15:05:13 +0200

    Export auxiliary functions

--- src/WorldDynamics.jl
+++ src/WorldDynamics.jl
@@ -4,7 +4,7 @@ include("functions.jl")
 include("plotvariables.jl")
 include("solvesystems.jl")
 
-export interpolate, plotvariables
+export interpolate, clip, switch, step, plotvariables, solve
 
 include("World2/World2.jl")
 include("World3/World3.jl")
