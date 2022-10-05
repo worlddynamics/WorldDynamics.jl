a3f4a93d0e0c407ce30faddbb4c569b864a97c9c
Paulo Bruno <pbrunosousa@gmail.com>
2022-10-05 15:23:54 +0200

    Create subsystem files in World2

---
+++ src/World2/naturalresources/subsystems.jl
@@ -0,0 +1,43 @@
+getinitialisations() = copy(inits)
+getparameters() = copy(params)
+gettables() = copy(tables)
+getranges() = copy(ranges)
+
+@variables t
+D = Differential(t)
+
+
+function natural_resources(; name, params=params, inits=inits, tables=tables, ranges=ranges)
+    @variables nrem(t)
+    @variables nrfr(t)
+    @variables nr(t) = inits[:nr]
+
+    @variables nrur(t)
+
+    eqs = [
+        nrem ~ interpolate(nrfr, tables[:nrem], ranges[:nrem])
+        nrfr ~ nr / inits[:nr]
+        D(nr) ~ -nrur
+    ]
+
+    ODESystem(eqs; name)
+end
+
+function natural_resources_usage_rate(; name, params=params, inits=inits, tables=tables, ranges=ranges)
+    @parameters nrun = params[:nrun]
+    @parameters nrun1 = params[:nrun1]
+    @parameters swt2 = params[:swt2]
+
+    @variables nrur(t)
+    @variables nrmm(t)
+
+    @variables p(t)
+    @variables msl(t)
+
+    eqs = [
+        nrur ~ p * clip(nrun, nrun1, swt2, t) * nrmm
+        nrmm ~ interpolate(msl, tables[:nrmm], ranges[:nrmm])
+    ]
+
+    ODESystem(eqs; name)
+end
