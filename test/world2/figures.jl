ae276e238f02897fd6c6f25629338fdd2d05e813
Paulo Bruno <pbrunosousa@gmail.com>
2022-10-06 12:39:38 +0200

    Add tests to new World2 figures

--- test/world2/figures.jl
+++ test/world2/figures.jl
@@ -1,5 +1,20 @@
 using WorldDynamics, Test, PlotlyJS
 
-@testset "World2" begin
-    @test isa(World2.fig_1(), PlotlyJS.SyncPlot)
+@testset verbose=true "World2" begin
+
+    @testset "Limits to Growth" begin
+        @test isa(World2.fig_1(), PlotlyJS.SyncPlot)
+        @test isa(World2.fig_2(), PlotlyJS.SyncPlot)
+        @test isa(World2.fig_3(), PlotlyJS.SyncPlot)
+        @test isa(World2.fig_4(), PlotlyJS.SyncPlot)
+        @test isa(World2.fig_5(), PlotlyJS.SyncPlot)
+        @test isa(World2.fig_6(), PlotlyJS.SyncPlot)
+        @test isa(World2.fig_7(), PlotlyJS.SyncPlot)
+        @test isa(World2.fig_8(), PlotlyJS.SyncPlot)
+        @test isa(World2.fig_9(), PlotlyJS.SyncPlot)
+        @test isa(World2.fig_10(), PlotlyJS.SyncPlot)
+        @test isa(World2.fig_11(), PlotlyJS.SyncPlot)
+        @test isa(World2.fig_12(), PlotlyJS.SyncPlot)
+    end
+
 end
