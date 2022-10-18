ea927fb545f5d284a02fd9ea3d922114f22eb8f3
Paulo Bruno <pbrunosousa@gmail.com>
2022-10-18 14:29:51 +0200

    Implement figure 7.27

--- src/World3/world3/plots.jl
+++ src/World3/world3/plots.jl
@@ -638,10 +638,81 @@ function fig_26(; kwargs...)
     return plotvariables(solution, (t, 1900, 2100), _variables_7(); title="Fig. 7.26", kwargs...)
 end
 
+
+function technological_costs(; name,
+    params=Dict([:icor1 => 3, :pyear => 1975]),
+    inits=Dict(),
+    tables=Dict([
+        :icor2 => (6.0, 3.3, 3.1, 3.06, 3.02, 3.0),
+        :copm => (2.0, 1.1, 1.05, 1.02, 1.01, 1.0),
+        :coym => (1.0, 1.1, 1.25, 1.5, 2.0),
+    ]),
+    ranges=Dict([:icor2 => (0.0, 1.0), :copm => (0.0, 1.0), :coym => (1.0, 9.0)]),
+)
+    @parameters icor1 = params[:icor1]
+    @parameters pyear = params[:pyear]
+
+    @variables nruf2(t), ppgf2(t), lyf2(t)
+    @variables icor2(t), copm(t), coym(t)
+
+    D = Differential(t)
+
+    eqs = [
+        icor2 ~ interpolate(nruf2, tables[:icor2], ranges[:icor2]) * copm * coym
+        copm ~ interpolate(ppgf2, tables[:copm], ranges[:copm])
+        coym ~ interpolate(lyf2, tables[:coym], ranges[:coym])
+    ]
+
+    return ODESystem(eqs; name)
+end
+
 """
     Reproduce Fig 7.27. The original figure is presented on Chapter 7.
 """
-fig_27(; kwargs...) = @info "This figure is not implemented yet."
+function fig_27(; kwargs...)
+    nr_tables = NonRenewable.gettables()
+    nr_tables[:fcaor2] = (1.0, 0.2, 0.1, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05)
+
+    agr_tables = Agriculture.gettables()
+    agr_tables[:llmy2] = (1.2, 1.0, 0.9, 0.8, 0.75, 0.7, 0.67, 0.64, 0.62, 0.6)
+    agr_tables[:lymap2] = (1.0, 1.0, 0.98, 0.95)
+
+    system = historicalrun(nonrenewable_tables=nr_tables, agriculture_tables=agr_tables)
+
+    @named nr = NonRenewable.non_renewable()
+    @named pp = Pollution.persistent_pollution()
+    @named ai = Agriculture.agricultural_inputs()
+    @named dlm = Agriculture.discontinuing_land_maintenance()
+    @named is = Capital.industrial_subsector()
+
+    @named rc = resource_control()
+    @named yc = yield_control()
+    @named pc = pollution_control()
+    @named tc = technological_costs()
+
+    connection_eqs = [
+        rc.nrur ~ nr.nrur
+        yc.fr ~ dlm.fr
+        pc.ppolx ~ pp.ppolx
+        tc.nruf2 ~ rc.nruf2
+        tc.lyf2 ~ yc.lyf2
+        tc.ppgf2 ~ pp.ppgf2
+    ]
+
+    new_equations = equations(system)
+
+    new_equations[204] = nr.nruf ~ clip(rc.nruf2, nr.nruf1, t, nr.pyear)
+    new_equations[177] = ai.lyf ~ clip(yc.lyf2, ai.lyf1, t, ai.pyear)
+    new_equations[215] = pp.ppgf ~ clip(pc.ppgf2, pp.ppgf1, t, pp.pyear)
+    new_equations[125] = is.icor ~ clip(tc.icor2, is.icor1, t, is.pyear)
+
+    @named _new_system = ODESystem(vcat(new_equations, connection_eqs))
+    @named new_system = ModelingToolkit.compose(_new_system, rc, yc, pc, tc)
+
+    solution = solve(new_system, (1900, 2100))
+
+    return plotvariables(solution, (t, 1900, 2100), _variables_7(); title="Fig. 7.27", kwargs...)
+end
 
 """
     Reproduce Fig 7.30. The original figure is presented on Chapter 7.
