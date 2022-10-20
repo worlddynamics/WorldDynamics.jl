b5c32468da5080cb5d38b528ea7ea57ddf4243a2
Paulo Bruno <pbrunosousa@gmail.com>
2022-10-20 15:08:45 +0200

    Add tests to World3-91

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,11 +5,13 @@ using WorldDynamics, Test
     @testset verbose=true "Scenarios" begin
         include("world2/scenarios.jl")
         include("world3/scenarios.jl")
+        include("world3_91/scenarios.jl")
     end
 
     @testset verbose=true "Figures" begin
         include("world2/figures.jl")
         include("world3/figures.jl")
+        include("world3_91/figures.jl")
     end
 
 end
