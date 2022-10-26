c1b91a69e73dbc4f7027fe9cb62d589ab8868e5b
universmile <hicham.lesfari@yahoo.com>
2022-10-26 16:43:09 +0200

    Metadata quality life

--- src/World2/qualitylife/subsystems.jl
+++ src/World2/qualitylife/subsystems.jl
@@ -2,18 +2,18 @@
 D = Differential(t)
 
 function quality_life(; name, params=_params, inits=_inits, tables=_tables, ranges=_ranges)
-    @parameters qls = params[:qls]
+    @parameters qls = params[:qls] [description = "Quality of life standard"]
 
-    @variables ql(t)
-    @variables qlm(t)
-    @variables qlc(t)
-    @variables qlf(t)
-    @variables qlp(t)
+    @variables ql(t) [description = "Quality of life"]
+    @variables qlm(t) [description = "Quality of life from material"]
+    @variables qlc(t) [description = "Quality of life from crowding"]
+    @variables qlf(t) [description = "Quality of life from food"]
+    @variables qlp(t) [description = "Quality of life from pollution"]
 
-    @variables msl(t)
-    @variables cr(t)
-    @variables fr(t)
-    @variables polr(t)
+    @variables msl(t) [description = "Material standard of living"]
+    @variables cr(t) [description = "Crowding ratio"]
+    @variables fr(t) [description = "Food ratio"]
+    @variables polr(t) [description = "Pollution ratio"]
 
     eqs = [
         ql ~ qls * qlm * qlc * qlf * qlp
