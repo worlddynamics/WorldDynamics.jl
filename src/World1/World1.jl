f6f2258bdf7c1175f0c6bc41519cb53b869706a1
Paulo Bruno <pbrunosousa@gmail.com>
2022-10-29 11:54:56 +0200

    Implement figure 5-8

--- src/World1/World1.jl
+++ src/World1/World1.jl
@@ -348,6 +348,25 @@ function _variables_7()
     return variables
 end
 
+function _variables_8()
+    @named w1 = world1()
+
+    variables = [
+        (w1.pop,  0,     4e9,   "Population"),
+        (w1.polr, 0,     20,    "Pollution"),
+        (w1.ci,   0,     8e9,   "Capital investment"),
+        (w1.fr,   0,     8,     "Food ratio"),
+        (w1.cr,   0,     0.8,   "Crowding ratio"),
+        (w1.msl,  0,     40,    "Material standard of living"),
+        (w1.qlm,  0,     8,     "Quality of life from materials"),
+        (w1.ql,   0,     120,   "Quality of life"),
+        (w1.ciaf, 0.25,  0.65,  "Capital in agriculture"),
+        (w1.nr,   500e9, 900e9, "Natural resources"),
+    ]
+
+    return variables
+end
+
 
 fig_std(; kwargs...) = plotvariables(standardrunsolution(), (t, 1900, 2100), _variables_std(); title="STD", showaxis=false, showlegend=false,kwargs...)
 
@@ -426,7 +445,14 @@ function fig_7(; kwargs...)
     plotvariables(sol, (t, 1900, 2100), _variables_7(); title="W1-7/5-7", showaxis=false, showlegend=false,kwargs...)
 end
 
+function fig_8(; kwargs...)
+    new_params = copy(_params)
+    new_params[:pdn] = 26.5
 
+    sol = solve(standard_run(params=new_params), (1900, 2100))
+
+    plotvariables(sol, (t, 1900, 2100), _variables_8(); title="W1-7/5-8", showaxis=false, showlegend=false,kwargs...)
+end
 
 
 end
