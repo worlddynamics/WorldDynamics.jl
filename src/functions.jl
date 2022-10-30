870ac6256be37d4e287006713fbaeb427069c78b
universmile <hicham.lesfari@yahoo.com>
2022-10-30 23:42:18 +0100

    Adapt docstrings to function definitions

--- src/functions.jl
+++ src/functions.jl
@@ -4,9 +4,9 @@ using IfElse
 interpolate(x, x₁, xₙ, y₁, yₙ) = y₁ + (x - x₁) * ((yₙ - y₁) / (xₙ - x₁))
 
 """
-   `interpolate(x, y, xs)`
+   `interpolate(x, yvalues, xrange)`
 
-Return the value of a function with input `x`, by linearly interpolating the function itself through the table `y` and the range `xs`. If `x` is out of the range, the value at the corresponding extremity is returned. This function correspond to the `TABHL` function in the `DYNAMO` language. This latter function receives a table (that is, `y`), a value (that is, `x`), a left and a right extreme of an interval (that is, `xs`), and an increment value.
+Return the value of a function with input `x`, by linearly interpolating the function itself through the table `yvalues` and the range `xrange`. If `x` is out of the range, the value at the corresponding extremity is returned. This function correspond to the `TABHL` function in the `DYNAMO` language. This latter function receives a table (that is, `y`), a value (that is, `x`), a left and a right extreme of an interval (that is, `xs`), and an increment value.
 """
 function interpolate(x, yvalues::Tuple{Vararg{Float64}}, xrange::Tuple{Float64, Float64})
     xvalues = LinRange(xrange[1], xrange[2], length(yvalues))
@@ -24,22 +24,22 @@ function interpolate(x, yvalues::Tuple{Vararg{Float64}}, xrange::Tuple{Float64, 
 end
 
 """
-   `clip(f1, f2, va, th)`
+   `clip(returnifgte, returniflt, inputvalue, threshold)`
 
-Return `f1` if the value `v` is greater than the threshold `th`, `f2` otherwise. This function correspond to the `CLIP` (also called `FIFGE`) function in the `DYNAMO` language.
+Return `returnifgte` if the value `inputvalue` is greater than the threshold `threshold`, `returniflt` otherwise. This function correspond to the `CLIP` (also called `FIFGE`) function in the `DYNAMO` language.
 """
 clip(returnifgte, returniflt, inputvalue, threshold) = IfElse.ifelse(inputvalue ≥ threshold, returnifgte, returniflt)
 
 """
-   `step(t, hght, sttm)`
+   `step(inputvalue, returnifgte, threshold)`
 
-Return `0` if the value `t` is smaller than the threshold `sttm`, `hght` otherwise. This function correspond to the `STEP` function in the `DYNAMO` language.
+Return `0` if the value `inputvalue` is smaller than the threshold `threshold`, `returnifgte` otherwise. This function correspond to the `STEP` function in the `DYNAMO` language.
 """
 step(inputvalue, returnifgte, threshold) = clip(returnifgte, zero(returnifgte), inputvalue, threshold)
 
 """
-   `switch(v1, v2, z)`
+   `switch(returnifzero, returnifnotzero, inputvalue)`
 
-Return `v1` if the value `z` is approximately `0` with tolerance `1e-16`, `v2` otherwise. This function correspond to the `SWITCH` (also called `FIFZE`) function in the `DYNAMO` language.
+Return `returnifzero` if the value `inputvalue` is approximately `0` with tolerance `1e-16`, `returnifnotzero` otherwise. This function correspond to the `SWITCH` (also called `FIFZE`) function in the `DYNAMO` language.
 """
 switch(returnifzero, returnifnotzero, inputvalue) = IfElse.ifelse(isapprox(inputvalue, zero(inputvalue); atol=1e-16), returnifzero, returnifnotzero)
