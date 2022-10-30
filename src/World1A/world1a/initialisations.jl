9484b2c72821ca4a02464a77eadc8f492b401354
Paulo Bruno <pbrunosousa@gmail.com>
2022-10-30 13:02:45 +0100

    Use World1 parameters as base values for World1A

--- src/World1A/world1a/initialisations.jl
+++ src/World1A/world1a/initialisations.jl
@@ -1,9 +1,5 @@
-_inits = Dict{Symbol, Float64}(
-    :pop => 1.65e9,
-    :nr => 900e9,
-    :ci => 0.4e9,
-    :pol => 0.2e9,
-    :ciaf => 0.3,
-)
+_inits = copy(World1._inits)
+
+_inits[:ciaf] = 0.3
 
 getinitialisations() = copy(_inits)
--- src/World1A/world1a/parameters.jl
+++ src/World1A/world1a/parameters.jl
@@ -1,18 +1,8 @@
-_params = Dict{Symbol, Float64}(
-    :brn => 0.045,
-    :brms => 1.0,
-    :msln => 0.7,
-    :nrun => 1.0,
-    :drn => 0.025,
-    :drms => 1.0,
-    :la => 135e6,
-    :pdn => 26.5,
-    :cigc => 1.0,
-    :cidn => 0.05,
-    :pols => 3.6e9,
-    :poln => 1.0,
-    :ciaft => 15.0,
-    :qls => 1.0,
-)
+_params = copy(World1._params)
+
+_params[:msln] = 0.7
+_params[:pdn] = 26.5
+_params[:cidn] = 0.05
+_params[:ciaft] = 15.0
 
 getparameters() = copy(_params)
