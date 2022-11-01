f1ca0ecb44b5af824105c9bb8ce06ad35e898eb8
universmile <hicham.lesfari@yahoo.com>
2022-11-01 16:11:46 +0100

    Switch from tuples to arrays for speedup

--- src/functions.jl
+++ src/functions.jl
@@ -8,21 +8,24 @@ interpolate(x, x₁, xₙ, y₁, yₙ) = y₁ + (x - x₁) * ((yₙ - y₁) / (x
 Returns the value of a function with input `x`, by linearly interpolating the function itself through the table `yvalues` and the range `xrange`. If `x` is out of the range, the value at the corresponding extremity is returned. This function corresponds to the `TABHL` function in the `DYNAMO` language. This latter function receives a table (that is, `yvalues`), a value (that is, `x`), a left and a right extreme of an interval (that is, `xrange`), and an increment value.
 """
 function interpolate(x, yvalues::Tuple{Vararg{Float64}}, xrange::Tuple{Float64, Float64})
-    xvalues = LinRange(xrange[1], xrange[2], length(yvalues))
-
-    # y gets the min y value if less than the min x range
-    #   or the max y value if greater than the max x range
-    y = (x < xrange[1]) * yvalues[1] + (x > xrange[2]) * yvalues[end]
-
-    # in case x is inside the range, y gets the interpolated value
-    for i ∈ 1:length(yvalues)-1
-        y += (x ≥ xvalues[i]) * (x ≤ xvalues[i+1]) * interpolate(x, xvalues[i], xvalues[i+1], yvalues[i], yvalues[i+1])
-    end
-
-    return y
+   xvalues = LinRange(xrange[1], xrange[2], length(yvalues))
+   interpolate(x, collect(yvalues), collect(xvalues))
 end
 
-function interpolate(x, pairs::Tuple{Vararg{<:NTuple{2, Float64}}})
+function interpolate(x, yvalues::Vector{Float64}, xvalues::Vector{Float64})
+   # y gets the min y value if less than the min x range
+   #   or the max y value if greater than the max x range
+   y = (x < xvalues[1]) * yvalues[1] + (x > xvalues[end]) * yvalues[end]
+
+   # in case x is inside the range, y gets the interpolated value
+   for i ∈ 1:length(yvalues)-1
+       y += (x ≥ xvalues[i]) * (x ≤ xvalues[i+1]) * interpolate(x, xvalues[i], xvalues[i+1], yvalues[i], yvalues[i+1])
+   end
+
+   return y
+end
+
+function interpolate(x, pairs::Vector{Tuple{Float64, Float64}})
    xvalues = map(t -> t[1], pairs)
    yvalues = map(t -> t[end], pairs)
    interpolate(x, yvalues, xvalues)
