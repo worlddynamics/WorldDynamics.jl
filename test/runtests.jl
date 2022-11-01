37ebba6824b20d1efb43effd3f79c1bec53c9795
Paulo Bruno <pbrunosousa@gmail.com>
2022-11-01 15:50:40 +0100

    Add tests to World1B

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,7 @@ using WorldDynamics, Test
     @testset verbose=true "Scenarios" begin
         include("world1/scenarios.jl")
         include("world1a/scenarios.jl")
+        include("world1b/scenarios.jl")
         include("world2/scenarios.jl")
         include("world3/scenarios.jl")
         include("world3_91/scenarios.jl")
@@ -14,6 +15,7 @@ using WorldDynamics, Test
     @testset verbose=true "Figures" begin
         include("world1/figures.jl")
         include("world1a/figures.jl")
+        include("world1b/figures.jl")
         include("world2/figures.jl")
         include("world3/figures.jl")
         include("world3_91/figures.jl")
