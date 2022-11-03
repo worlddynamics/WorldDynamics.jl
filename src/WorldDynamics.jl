2489ad2f43ed7d42d1c595a7e5a63670e365e936
universmile <hicham.lesfari@yahoo.com>
2022-11-03 21:47:48 +0100

    Export functions

--- src/WorldDynamics.jl
+++ src/WorldDynamics.jl
@@ -6,16 +6,15 @@ include("functions.jl")
 
 export plotvariables
 export solve
-export interpolate, clip, switch, step, step2, ramp
+export interpolate, clip, switch, ramp, pulse
 
 using ModelingToolkit
 
 @register interpolate(x, y::Tuple{Vararg{Float64}}, xs::Tuple{Float64, Float64})
-@register clip(f1, f2, va, th)
-@register switch(v1, v2, z)
-@register WorldDynamics.step(t, hght, sttm)
-@register step2(t, hght, sttm1, sttm2)
-@register ramp(t, slope, startslope, endslope)
+@register clip(returnifgte, returniflt, inputvalue, threshold)
+@register switch(returnifzero, returnifnotzero, inputvalue)
+@register WorldDynamics.step(inputvalue, returnifgte, threshold)
+@register ramp(inputvalue, slope, startslope, endslope)
 @register pulse(inputvalue, start, width)
 
 include("World2/World2.jl")
