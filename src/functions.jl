6a1a121f8d1e8011354ae4813fd6eb58f7d526a2
universmile <hicham.lesfari@yahoo.com>
2022-11-04 17:59:34 +0100

    Restore blank lines for functions

--- src/functions.jl
+++ src/functions.jl
@@ -5,6 +5,7 @@ interpolate(x, x₁, xₙ, y₁, yₙ) = y₁ + (x - x₁) * ((yₙ - y₁) / (x
 
 """
    `interpolate(x, yvalues, xrange)`
+
 Returns the value of a function with input `x`, by linearly interpolating the function itself through the table `yvalues` and the range `xrange`. If `x` is out of the range, the value at the corresponding extremity is returned. This function corresponds to the `TABHL` function in the `DYNAMO` language. This latter function receives a table (that is, `yvalues`), a value (that is, `x`), a left and a right extreme of an interval (that is, `xrange`), and an increment value.
 """
 function interpolate(x, yvalues::Tuple{Vararg{Float64}}, xrange::Tuple{Float64, Float64})
@@ -33,18 +34,21 @@ end
 
 """
    `clip(returnifgte, returniflt, inputvalue, threshold)`
+
 Returns `returnifgte` if the value `inputvalue` is greater than the threshold `threshold`, `returniflt` otherwise. This function corresponds to the `CLIP` (also called `FIFGE`) function in the `DYNAMO` language.
 """
 clip(returnifgte, returniflt, inputvalue, threshold) = IfElse.ifelse(inputvalue ≥ threshold, returnifgte, returniflt)
 
 """
    `step(inputvalue, returnifgte, threshold)`
+
 Returns `0` if the value `inputvalue` is smaller than the threshold `threshold`, `returnifgte` otherwise. This function corresponds to the `STEP` function in the `DYNAMO` and `VENSIM` languages.
 """
 step(inputvalue, returnifgte, threshold) = clip(returnifgte, zero(returnifgte), inputvalue, threshold)
 
 """
    `switch(returnifzero, returnifnotzero, inputvalue)`
+
 Returns `returnifzero` if the value `inputvalue` is approximately `0` with tolerance `1e-16`, `returnifnotzero` otherwise. This function corresponds to the `SWITCH` (also called `FIFZE`) function in the `DYNAMO` language.
 """
 switch(returnifzero, returnifnotzero, inputvalue) = IfElse.ifelse(isapprox(inputvalue, zero(inputvalue); atol=1e-16), returnifzero, returnifnotzero)
