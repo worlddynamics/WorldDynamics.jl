d513ded4bc941fc11bd55f5cd9ae3146793ace4f
universmile <hicham.lesfari@yahoo.com>
2022-11-17 00:23:29 +0100

    Implement Figure 3 (GL scenario)

--- src/Earth4All/plots.jl
+++ src/Earth4All/plots.jl
@@ -22,3 +22,13 @@ variables_2 = [
     (Govmnt_gross_income_as_share_of_NI,    0,  0.4,    "Govmnt gross income (as share of NI)"),
 ]
 plotvariables(solution,(t, 1980, 2100), variables_2; title="2. Human footprint - World 1980 to 2020")
+
+variables_3 = [
+    (Worker_disposable_income_k_dollar_per_p_per_y, 0,  50, "Worker disposable income k$/p/y"),
+    (CO2_emissions_per_person_tCO2_per_y,   0,  6,  "CO2 emissions per person tCO2/y"),
+    (Crop_use_per_person_t_crop_per_p_per_y,    0,  1,   "Crop use per person t-crop/p/y"),
+    (Cost_of_energy_G_dollar_per_y, 0,  6000,    "Cost of energy G$/y"),
+    (Energy_use_per_person_toe_per_p_per_y, 0,  4,   "Energy use per person toe/p/y"),
+    (Forestry_land_Mha, 0,   2000, "Forestry land Mha"),
+]
+plotvariables(solution,(t, 1980, 2100), variables_3; title="3. Consumption  - World 1980 to 2100")
