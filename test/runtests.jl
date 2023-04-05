fdd9a44b2ff09cef42110e8544b4f4527b3ee07c
aurorarossi <aurora98rossi@gmail.com>
2023-04-05 21:24:30 +0200

    Add solvesystems tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,18 @@
 using WorldDynamics, Test
 
-@testset verbose=true "WorldDynamics.jl" begin
+@testset verbose = true "WorldDynamics.jl" begin
 
-    @testset verbose=true "Scenarios" begin
+    @testset verbose = true "Scenarios" begin
         include("world2/scenarios.jl")
         include("world3/scenarios.jl")
         include("world3_91/scenarios.jl")
     end
 
-    @testset verbose=true "Figures" begin
+    @testset verbose = true "Figures" begin
         include("world2/figures.jl")
         include("world3/figures.jl")
         include("world3_91/figures.jl")
     end
 
+    include("solvesystems.jl")
 end
