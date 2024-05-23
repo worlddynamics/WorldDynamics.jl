4d01770cf94c5680b4887a1f5a3ff2090a05c3e7
piluc <pierluigi.crescenzi@gssi.it>
2024-05-23 20:04:07 +0200

    Update README

--- src/Capital.jl
+++ src/Capital.jl
@@ -7,7 +7,5 @@ include("capital/tables.jl")
 include("capital/parameters.jl")
 include("capital/initialisations.jl")
 include("capital/subsystems.jl")
-include("capital/scenarios.jl")
-include("capital/plots.jl")
 
 end
--- src/Resource.jl
+++ src/Resource.jl
@@ -7,7 +7,5 @@ include("resource/tables.jl")
 include("resource/parameters.jl")
 include("resource/initialisations.jl")
 include("resource/subsystems.jl")
-include("resource/scenarios.jl")
-include("resource/plots.jl")
 
 end
